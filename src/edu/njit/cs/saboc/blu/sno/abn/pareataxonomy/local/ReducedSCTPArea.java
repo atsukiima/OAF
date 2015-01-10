02066ef6a03f77baf5b5b93ddde46f8ae5ab392e
Den <Den@Den-PC.home>
2015-01-10 17:40:08 -0500

    Added skeleton for reduced PArea interface

--- src/edu/njit/cs/saboc/blu/sno/abn/pareataxonomy/local/ReducedSCTPArea.java
+++ src/edu/njit/cs/saboc/blu/sno/abn/pareataxonomy/local/ReducedSCTPArea.java
@@ -1,5 +1,6 @@
 package edu.njit.cs.saboc.blu.sno.abn.pareataxonomy.local;
 
+import SnomedShared.Concept;
 import edu.njit.cs.saboc.blu.core.abn.reduced.ReducingGroup;
 import edu.njit.cs.saboc.blu.sno.datastructure.hierarchy.SCTConceptHierarchy;
 import java.util.HashSet;
@@ -8,7 +9,8 @@ import java.util.HashSet;
  *
  * @author Chris O
  */
-public class ReducedSCTPArea extends SCTPArea implements ReducingGroup<SCTPArea> {
+public class ReducedSCTPArea extends SCTPArea implements ReducingGroup<Concept, SCTPArea> {
+
     
     private HashSet<SCTPArea> reducedPAreas;
     
@@ -20,5 +22,13 @@ public class ReducedSCTPArea extends SCTPArea implements ReducingGroup<SCTPArea>
     
     public HashSet<SCTPArea> getReducedGroups() {
         return reducedPAreas;
+    }
+    
+    public HashSet<Concept> getAllGroupsConcepts() {
+        return new HashSet<Concept>();
+    }
+
+    public HashSet<Concept> getAllGroupsSourceConcepts() {
+        return new HashSet<Concept>();
     }
 }
