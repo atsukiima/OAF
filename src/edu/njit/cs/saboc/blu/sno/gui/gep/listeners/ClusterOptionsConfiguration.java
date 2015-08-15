bfb0d7ce58dffc2f61ad86f8acd3ee0343abd062
chriso <cro3@njit.edu>
2015-08-14 20:18:50 -0400

    Changed GEP configuration set up.

--- src/edu/njit/cs/saboc/blu/sno/gui/gep/listeners/ClusterOptionsConfiguration.java
+++ src/edu/njit/cs/saboc/blu/sno/gui/gep/listeners/ClusterOptionsConfiguration.java
@@ -1,13 +1,9 @@
 package edu.njit.cs.saboc.blu.sno.gui.gep.listeners;
 
-import SnomedShared.overlapping.ClusterSummary;
-import edu.njit.cs.saboc.blu.core.gui.gep.panels.GroupOptionsPanelActionListener;
-import edu.njit.cs.saboc.blu.core.gui.gep.panels.GroupOptionsPanelConfiguration;
-import edu.njit.cs.saboc.blu.sno.abn.tan.TANGenerator;
+import edu.njit.cs.saboc.blu.core.gui.gep.panels.BLUGraphConfiguration;
+import edu.njit.cs.saboc.blu.core.gui.gep.panels.details.AbstractGroupPanel;
 import edu.njit.cs.saboc.blu.sno.abn.tan.TribalAbstractionNetwork;
-import edu.njit.cs.saboc.blu.sno.datastructure.hierarchy.SCTConceptHierarchy;
 import edu.njit.cs.saboc.blu.sno.gui.abnselection.SCTDisplayFrameListener;
-import edu.njit.cs.saboc.blu.sno.gui.dialogs.ConceptGroupDetailsDialog;
 import edu.njit.cs.saboc.blu.sno.gui.graphframe.ClusterInternalGraphFrame;
 import javax.swing.JFrame;
 
@@ -15,39 +11,23 @@ import javax.swing.JFrame;
  *
  * @author Chris
  */
-public class ClusterOptionsConfiguration extends GroupOptionsPanelConfiguration {
+public class ClusterOptionsConfiguration extends BLUGraphConfiguration {
 
     public ClusterOptionsConfiguration(
             final JFrame parentFrame, 
             final ClusterInternalGraphFrame graphFrame,
             final TribalAbstractionNetwork tan, 
             final SCTDisplayFrameListener displayListener) {
-        
-        super.enableButtonWithAction(0, new GroupOptionsPanelActionListener<ClusterSummary>() {
-            public void actionPerformedOn(ClusterSummary cluster) {
-                
-               ConceptGroupDetailsDialog dialog = new ConceptGroupDetailsDialog(cluster, tan,
-                            ConceptGroupDetailsDialog.DialogType.Cluster, displayListener);
-            }
-        });
-        
-        super.enableButtonWithAction(1, new GroupOptionsPanelActionListener<ClusterSummary>() {
-            public void actionPerformedOn(ClusterSummary cluster) {
-                displayListener.addNewBrowserFrame(cluster.getRoot(), tan.getDataSource());
-            }
-        });
-                
-        super.enableButtonWithAction(4, new GroupOptionsPanelActionListener<ClusterSummary>() {
-            public void actionPerformedOn(ClusterSummary cluster) {
-                SCTConceptHierarchy hierarchy = tan.getDataSource().getClusterConceptHierarchy(tan, cluster);
 
-                TribalAbstractionNetwork chd = TANGenerator.createTANFromConceptHierarchy(
-                        cluster.getRoot(),
-                        tan.getSCTVersion(),
-                        hierarchy);
+    }
+    
+    @Override
+    public boolean hasGroupDetailsPanel() {
+        return false;
+    }
 
-                displayListener.addNewClusterGraphFrame(chd, true, false);
-            }
-        });
+    @Override
+    public AbstractGroupPanel createGroupDetailsPanel() {
+        return null;
     }
 }
