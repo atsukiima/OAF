2a33822f571dc6f307e0c2b1c49f2639b8a0d7fc
chriso <cro3@njit.edu>
2017-01-17 14:29:16 -0500

    Converted all non-diff abns to use multiabn graph frame

--- src/edu/njit/cs/saboc/blu/sno/gui/abnselection/SCTAbNFrameManager.java
+++ src/edu/njit/cs/saboc/blu/sno/gui/abnselection/SCTAbNFrameManager.java
@@ -2,27 +2,19 @@ package edu.njit.cs.saboc.blu.sno.gui.abnselection;
 
 import edu.njit.cs.saboc.blu.core.abn.disjoint.DisjointAbstractionNetwork;
 import edu.njit.cs.saboc.blu.core.abn.disjoint.DisjointNode;
+import edu.njit.cs.saboc.blu.core.abn.pareataxonomy.DisjointPArea;
 import edu.njit.cs.saboc.blu.core.abn.pareataxonomy.PArea;
 import edu.njit.cs.saboc.blu.core.abn.pareataxonomy.PAreaTaxonomy;
 import edu.njit.cs.saboc.blu.core.abn.pareataxonomy.diff.DiffPAreaTaxonomy;
 import edu.njit.cs.saboc.blu.core.abn.tan.Cluster;
 import edu.njit.cs.saboc.blu.core.abn.tan.ClusterTribalAbstractionNetwork;
 import edu.njit.cs.saboc.blu.core.abn.targetbased.TargetAbstractionNetwork;
-import edu.njit.cs.saboc.blu.core.gui.gep.panels.configuration.DisjointAbNConfiguration;
-import edu.njit.cs.saboc.blu.core.gui.gep.panels.configuration.PartitionedAbNConfiguration;
+import edu.njit.cs.saboc.blu.core.graph.tan.DisjointCluster;
 import edu.njit.cs.saboc.blu.core.gui.graphframe.AbNDisplayManager;
 import edu.njit.cs.saboc.blu.core.gui.graphframe.FrameCreationAction;
-import edu.njit.cs.saboc.blu.core.gui.graphframe.ClusterInternalGraphFrame;
-import edu.njit.cs.saboc.blu.core.gui.graphframe.DisjointAbNInternalGraphFrame;
-import edu.njit.cs.saboc.blu.core.gui.graphframe.DisjointAbNInternalGraphFrame.DisjointAbNConfigurationConfiguration;
-import edu.njit.cs.saboc.blu.sno.gui.gep.panels.disjointpareataxonomy.configuration.SCTDisjointPAreaTaxonomyConfigurationFactory;
-import edu.njit.cs.saboc.blu.sno.gui.gep.panels.disjointtan.configuration.SCTDisjointTANConfigurationFactory;
-import edu.njit.cs.saboc.blu.sno.gui.gep.panels.pareataxonomy.configuration.SCTPAreaTaxonomyConfigurationFactory;
-import edu.njit.cs.saboc.blu.sno.gui.gep.panels.tan.configuration.SCTTANConfiguration;
-import edu.njit.cs.saboc.blu.sno.gui.gep.panels.tan.configuration.SCTTANConfigurationFactory;
-import edu.njit.cs.saboc.blu.sno.gui.gep.panels.tan.reports.SCTTANReportDialog;
-import edu.njit.cs.saboc.blu.sno.gui.graphframe.PAreaInternalGraphFrame;
+import edu.njit.cs.saboc.blu.core.gui.graphframe.multiabn.MultiAbNGraphFrame;
 import edu.njit.cs.saboc.blu.sno.gui.graphframe.SCTDiffPAreaTaxonomyGraphFrame;
+import edu.njit.cs.saboc.blu.sno.gui.graphframe.initializers.SCTMultiAbNGraphFrameInitializers;
 import javax.swing.JFrame;
 
 /**
@@ -31,98 +23,79 @@ import javax.swing.JFrame;
  */
 public class SCTAbNFrameManager extends AbNDisplayManager  {
     
+        
     private final JFrame mainFrame;
     
     public SCTAbNFrameManager(JFrame mainFrame, FrameCreationAction fca) {
         super(fca);
-        
+
         this.mainFrame = mainFrame;
     }
     
     @Override
-    public void displayPAreaTaxonomy(PAreaTaxonomy data) {
-        PAreaInternalGraphFrame igf = new PAreaInternalGraphFrame(mainFrame, data, this);
+    public void displayPAreaTaxonomy(PAreaTaxonomy taxonomy) {
+        MultiAbNGraphFrame graphFrame = new MultiAbNGraphFrame(mainFrame, new SCTMultiAbNGraphFrameInitializers());
+        graphFrame.displayPAreaTaxonomy(taxonomy);
 
-        super.getFrameCreationAction().displayFrame(igf);
+        this.getFrameCreationAction().displayFrame(graphFrame);
     }
-
+        
+    
     @Override
     public void displayTribalAbstractionNetwork(ClusterTribalAbstractionNetwork tan) {
-        
-        ClusterInternalGraphFrame cigf = new ClusterInternalGraphFrame(
-                mainFrame,
-                
-                (clusterTAN, displayManager) -> {
-                    return new SCTTANConfigurationFactory().createConfiguration(clusterTAN, displayManager);
-                },
-                
-                (config) -> {
+        MultiAbNGraphFrame graphFrame = new MultiAbNGraphFrame(mainFrame, new SCTMultiAbNGraphFrameInitializers());
+        graphFrame.displayTAN(tan);
 
-                    SCTTANReportDialog reportDialog = new SCTTANReportDialog((SCTTANConfiguration)config);
-                    reportDialog.showReports(config.getAbstractionNetwork());
-                    reportDialog.setModal(true);
-
-                    reportDialog.setVisible(true);
-                }, 
-                
-                this);
-
-        cigf.displayClusterTAN(tan);
-        
-        super.getFrameCreationAction().displayFrame(cigf);
+        this.getFrameCreationAction().displayFrame(graphFrame);
     }
-    
-    
+
     @Override
     public void displayDisjointPAreaTaxonomy(
             DisjointAbstractionNetwork<DisjointNode<PArea>, PAreaTaxonomy<PArea>, PArea> disjointTaxonomy) {
-        
-        DisjointAbNInternalGraphFrame frame = new DisjointAbNInternalGraphFrame(
-                mainFrame,
-                new DisjointAbNConfigurationConfiguration() {
 
-                    @Override
-                    public DisjointAbNConfiguration getDisjointAbNConfiguration(DisjointAbstractionNetwork disjointAbN, AbNDisplayManager displayListener) {
-                        return new SCTDisjointPAreaTaxonomyConfigurationFactory().createConfiguration(disjointAbN, displayListener);
-                    }
+        MultiAbNGraphFrame graphFrame = new MultiAbNGraphFrame(mainFrame, new SCTMultiAbNGraphFrameInitializers());
+        graphFrame.displayDisjointPAreaTaxonomy(
+                (DisjointAbstractionNetwork<DisjointPArea, PAreaTaxonomy<PArea>, PArea>)(DisjointAbstractionNetwork<?, ?, ?>)disjointTaxonomy);
 
-                    @Override
-                    public PartitionedAbNConfiguration getParentAbNConfiguration(DisjointAbstractionNetwork disjointAbN, AbNDisplayManager displayListener) {
-                        return new SCTPAreaTaxonomyConfigurationFactory().createConfiguration((PAreaTaxonomy)disjointAbN.getParentAbstractionNetwork(), displayListener);
-                    }
-
-                }, 
-                this);
-
-        frame.displayDisjointAbN(disjointTaxonomy);
-        
-        super.getFrameCreationAction().displayFrame(frame);
+        this.getFrameCreationAction().displayFrame(graphFrame);
     }
     
     @Override
     public void displayDisjointTribalAbstractionNetwork(
             DisjointAbstractionNetwork<DisjointNode<Cluster>, ClusterTribalAbstractionNetwork<Cluster>, Cluster> disjointTAN) {
         
-        DisjointAbNInternalGraphFrame frame = new DisjointAbNInternalGraphFrame(
-                mainFrame,
-                new DisjointAbNConfigurationConfiguration() {
+        MultiAbNGraphFrame graphFrame = new MultiAbNGraphFrame(mainFrame, new SCTMultiAbNGraphFrameInitializers());
+        graphFrame.displayDisjointTAN(
+                (DisjointAbstractionNetwork<DisjointCluster, ClusterTribalAbstractionNetwork<Cluster>, Cluster>)(DisjointAbstractionNetwork<?, ?, ?>)
+                        disjointTAN);
 
-                    @Override
-                    public DisjointAbNConfiguration getDisjointAbNConfiguration(DisjointAbstractionNetwork disjointAbN, AbNDisplayManager displayListener) {
-                        return new SCTDisjointTANConfigurationFactory().createConfiguration(disjointAbN, displayListener);
-                    }
-
-                    @Override
-                    public PartitionedAbNConfiguration getParentAbNConfiguration(DisjointAbstractionNetwork disjointAbN, AbNDisplayManager displayListener) {
-                        return new SCTTANConfigurationFactory().createConfiguration((ClusterTribalAbstractionNetwork)disjointAbN.getParentAbstractionNetwork(), displayListener);
-                    }
-                }, 
-                this);
-        
-        frame.displayDisjointAbN(disjointTAN);
-        
-        super.getFrameCreationAction().displayFrame(frame);
+        this.getFrameCreationAction().displayFrame(graphFrame);
     }
+
+    @Override
+    public void displayTargetAbstractionNetwork(TargetAbstractionNetwork targetAbN) {
+        MultiAbNGraphFrame graphFrame = new MultiAbNGraphFrame(mainFrame, new SCTMultiAbNGraphFrameInitializers());
+        graphFrame.displayTargetAbstractionNewtork(targetAbN);
+
+        this.getFrameCreationAction().displayFrame(graphFrame);
+    }
+
+    @Override
+    public void displayAreaTaxonomy(PAreaTaxonomy taxonomy) {
+        MultiAbNGraphFrame graphFrame = new MultiAbNGraphFrame(mainFrame, new SCTMultiAbNGraphFrameInitializers());
+        graphFrame.displayAreaTaxonomy(taxonomy);
+
+        this.getFrameCreationAction().displayFrame(graphFrame);
+    }
+
+    @Override
+    public void displayBandTribalAbstractionNetwork(ClusterTribalAbstractionNetwork tan) {
+        MultiAbNGraphFrame graphFrame = new MultiAbNGraphFrame(mainFrame, new SCTMultiAbNGraphFrameInitializers());
+        graphFrame.displayBandTAN(tan);
+
+        this.getFrameCreationAction().displayFrame(graphFrame);
+    }
+    
     
     @Override
     public void displayDiffPAreaTaxonomy(DiffPAreaTaxonomy diffTaxonomy) {
@@ -130,20 +103,5 @@ public class SCTAbNFrameManager extends AbNDisplayManager  {
         frame.displayDiffPAreaTaxonomy(diffTaxonomy);
         
         super.getFrameCreationAction().displayFrame(frame);
-    }
-
-    @Override
-    public void displayTargetAbstractionNetwork(TargetAbstractionNetwork targetAbN) {
-        
-    }
-
-    @Override
-    public void displayAreaTaxonomy(PAreaTaxonomy taxonomy) {
-        
-    }
-
-    @Override
-    public void displayBandTribalAbstractionNetwork(ClusterTribalAbstractionNetwork tan) {
-        
     }
 }
---
+++ src/edu/njit/cs/saboc/blu/sno/gui/graphframe/initializers/SCTMultiAbNGraphFrameInitializers.java
@@ -0,0 +1,229 @@
+package edu.njit.cs.saboc.blu.sno.gui.graphframe.initializers;
+
+import edu.njit.cs.saboc.blu.core.abn.disjoint.DisjointAbstractionNetwork;
+import edu.njit.cs.saboc.blu.core.abn.pareataxonomy.PAreaTaxonomy;
+import edu.njit.cs.saboc.blu.core.abn.tan.ClusterTribalAbstractionNetwork;
+import edu.njit.cs.saboc.blu.core.abn.targetbased.TargetAbstractionNetwork;
+import edu.njit.cs.saboc.blu.core.gui.gep.AbNExplorationPanelGUIInitializer;
+import edu.njit.cs.saboc.blu.core.gui.gep.DisjointAbNExplorationPanelInitializer;
+import edu.njit.cs.saboc.blu.core.gui.gep.panels.configuration.AbNConfiguration;
+import edu.njit.cs.saboc.blu.core.gui.gep.panels.configuration.DisjointAbNConfiguration;
+import edu.njit.cs.saboc.blu.core.gui.gep.panels.details.pareataxonomy.configuration.PAreaTaxonomyConfiguration;
+import edu.njit.cs.saboc.blu.core.gui.gep.panels.details.tan.TANConfiguration;
+import edu.njit.cs.saboc.blu.core.gui.gep.panels.details.targetbased.configuration.TargetAbNConfiguration;
+import edu.njit.cs.saboc.blu.core.gui.graphframe.AbNDisplayManager;
+import edu.njit.cs.saboc.blu.core.gui.graphframe.multiabn.AbNGraphFrameInitializers;
+import edu.njit.cs.saboc.blu.core.gui.graphframe.multiabn.GraphFrameInitializer;
+import edu.njit.cs.saboc.blu.core.gui.graphframe.multiabn.MultiAbNGraphFrame;
+import edu.njit.cs.saboc.blu.core.gui.graphframe.multiabn.TaskBarPanel;
+import edu.njit.cs.saboc.blu.core.gui.graphframe.multiabn.initializers.AreaTaxonomyInitializer;
+import edu.njit.cs.saboc.blu.core.gui.graphframe.multiabn.initializers.BandTANInitializer;
+import edu.njit.cs.saboc.blu.core.gui.graphframe.multiabn.initializers.DisjointAbNInitializer;
+import edu.njit.cs.saboc.blu.core.gui.graphframe.multiabn.initializers.PAreaTaxonomyInitializer;
+import edu.njit.cs.saboc.blu.core.gui.graphframe.multiabn.initializers.TANInitializer;
+import edu.njit.cs.saboc.blu.core.gui.graphframe.multiabn.initializers.TargetAbNInitializer;
+import edu.njit.cs.saboc.blu.sno.gui.gep.panels.disjointpareataxonomy.configuration.SCTDisjointPAreaTaxonomyConfigurationFactory;
+import edu.njit.cs.saboc.blu.sno.gui.gep.panels.disjointtan.configuration.SCTDisjointTANConfigurationFactory;
+import edu.njit.cs.saboc.blu.sno.gui.gep.panels.pareataxonomy.configuration.SCTPAreaTaxonomyConfiguration;
+import edu.njit.cs.saboc.blu.sno.gui.gep.panels.pareataxonomy.configuration.SCTPAreaTaxonomyConfigurationFactory;
+import edu.njit.cs.saboc.blu.sno.gui.gep.panels.pareataxonomy.reports.SCTPAreaTaxonomyReportDialog;
+import edu.njit.cs.saboc.blu.sno.gui.gep.panels.tan.configuration.SCTTANConfiguration;
+import edu.njit.cs.saboc.blu.sno.gui.gep.panels.tan.configuration.SCTTANConfigurationFactory;
+import edu.njit.cs.saboc.blu.sno.gui.gep.panels.tan.reports.SCTTANReportDialog;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Chris O
+ */
+public class SCTMultiAbNGraphFrameInitializers implements AbNGraphFrameInitializers {
+    
+    private JButton createPAreaTaxonomyReportsBtn(SCTPAreaTaxonomyConfiguration config) {
+        
+        JButton sctTaxonomyReportsBtn = new JButton("Reports and Metrics");
+
+        sctTaxonomyReportsBtn.addActionListener((ae) -> {
+
+            if (config.getPAreaTaxonomy().isAggregated()) {
+
+            } else {
+                SCTPAreaTaxonomyReportDialog reportDialog = new SCTPAreaTaxonomyReportDialog(config);
+                reportDialog.showReports(config.getPAreaTaxonomy());
+
+                reportDialog.setModal(true);
+                reportDialog.setVisible(true);
+            }
+        });
+
+        return sctTaxonomyReportsBtn;
+    }
+    
+    private JButton createTANReportsBtn(SCTTANConfiguration config) {
+        
+        JButton sctTANReportsBtn = new JButton("Reports and Metrics");
+
+        sctTANReportsBtn.addActionListener((ae) -> {
+           
+                    SCTTANReportDialog reportDialog = new SCTTANReportDialog((SCTTANConfiguration)config);
+                    reportDialog.showReports(config.getAbstractionNetwork());
+                    reportDialog.setModal(true);
+
+                    reportDialog.setVisible(true);
+        });
+
+        return sctTANReportsBtn;
+    }
+
+    @Override
+    public GraphFrameInitializer<PAreaTaxonomy, PAreaTaxonomyConfiguration> getPAreaTaxonomyInitializer() {
+        
+        return new PAreaTaxonomyInitializer() {
+
+            @Override
+            public TaskBarPanel getTaskBar(MultiAbNGraphFrame graphFrame, PAreaTaxonomyConfiguration config) {
+                TaskBarPanel panel = super.getTaskBar(graphFrame, config); 
+                
+                panel.addReportButtonToMenu(createPAreaTaxonomyReportsBtn((SCTPAreaTaxonomyConfiguration)config));
+                
+                return panel;
+            }
+
+            @Override
+            public AbNConfiguration getConfiguration(PAreaTaxonomy abn, AbNDisplayManager displayManager) {
+                return new SCTPAreaTaxonomyConfigurationFactory().createConfiguration(abn, displayManager);
+            }
+        };
+    }
+
+    @Override
+    public GraphFrameInitializer<PAreaTaxonomy, PAreaTaxonomyConfiguration> getAreaTaxonomyInitializer() {
+        return new AreaTaxonomyInitializer() {
+            
+            @Override
+            public TaskBarPanel getTaskBar(MultiAbNGraphFrame graphFrame, PAreaTaxonomyConfiguration config) {
+                TaskBarPanel panel = super.getTaskBar(graphFrame, config); 
+                
+                panel.addReportButtonToMenu(createPAreaTaxonomyReportsBtn((SCTPAreaTaxonomyConfiguration)config));
+                
+                return panel;
+            }
+            
+            @Override
+            public AbNConfiguration getConfiguration(PAreaTaxonomy abn, AbNDisplayManager displayManager) {
+                return new SCTPAreaTaxonomyConfigurationFactory().createConfiguration(abn, displayManager);
+            }
+        };
+    }
+
+    @Override
+    public GraphFrameInitializer<ClusterTribalAbstractionNetwork, TANConfiguration> getTANInitializer() {
+        
+        return new TANInitializer() {
+            
+            @Override
+            public TaskBarPanel getTaskBar(MultiAbNGraphFrame graphFrame, TANConfiguration config) {
+                TaskBarPanel panel = super.getTaskBar(graphFrame, config); 
+                
+                panel.addReportButtonToMenu(createTANReportsBtn((SCTTANConfiguration)config));
+                
+                return panel;
+            }
+
+            @Override
+            public AbNConfiguration getConfiguration(ClusterTribalAbstractionNetwork abn, AbNDisplayManager displayManager) {
+                return new SCTTANConfigurationFactory().createConfiguration(abn, displayManager);
+            }
+        };
+    }
+    
+    
+    @Override
+    public GraphFrameInitializer<ClusterTribalAbstractionNetwork, TANConfiguration> getBandTANInitializer() {
+        return new BandTANInitializer() {
+            
+            @Override
+            public TaskBarPanel getTaskBar(MultiAbNGraphFrame graphFrame, TANConfiguration config) {
+                TaskBarPanel panel = super.getTaskBar(graphFrame, config); 
+                
+                panel.addReportButtonToMenu(createTANReportsBtn((SCTTANConfiguration)config));
+                
+                return panel;
+            }
+
+            @Override
+            public AbNConfiguration getConfiguration(ClusterTribalAbstractionNetwork abn, AbNDisplayManager displayManager) {
+                return new SCTTANConfigurationFactory().createConfiguration(abn, displayManager);
+            }
+        };
+    }
+    
+
+    @Override
+    public GraphFrameInitializer<TargetAbstractionNetwork, TargetAbNConfiguration> getTargetAbNInitializer() {
+        return new TargetAbNInitializer() {
+
+            @Override
+            public AbNConfiguration getConfiguration(TargetAbstractionNetwork abn, AbNDisplayManager displayManager) {
+                return null;
+            }
+        };
+    }
+
+    @Override
+    public GraphFrameInitializer<DisjointAbstractionNetwork, DisjointAbNConfiguration> getDisjointPAreaTaxonomyInitializer() {
+        
+        return new DisjointAbNInitializer() {
+
+            @Override
+            public AbNConfiguration getConfiguration(DisjointAbstractionNetwork abn, AbNDisplayManager displayManager) {
+                return new SCTDisjointPAreaTaxonomyConfigurationFactory().createConfiguration(abn, displayManager);
+            }
+
+            @Override
+            public AbNExplorationPanelGUIInitializer getExplorationGUIInitializer(DisjointAbNConfiguration config) {
+                
+                PAreaTaxonomy taxonomy = (PAreaTaxonomy)config.getAbstractionNetwork().getParentAbstractionNetwork();
+                
+                return new DisjointAbNExplorationPanelInitializer(
+                        config,
+                        new SCTPAreaTaxonomyConfigurationFactory().createConfiguration(taxonomy, config.getUIConfiguration().getAbNDisplayManager()),
+                        (bound) -> {
+                            DisjointAbstractionNetwork disjointAbN = config.getAbstractionNetwork().getAggregated(bound);
+                            config.getUIConfiguration().getAbNDisplayManager().displayDisjointPAreaTaxonomy(disjointAbN);
+                        },
+                        
+                        (disjointAbN) -> {
+                            config.getUIConfiguration().getAbNDisplayManager().displayDisjointPAreaTaxonomy(disjointAbN);
+                        });
+            }
+        };
+    }
+
+    @Override
+    public GraphFrameInitializer<DisjointAbstractionNetwork, DisjointAbNConfiguration> getDisjointTANInitializer() {
+        return new DisjointAbNInitializer() {
+
+            @Override
+            public AbNConfiguration getConfiguration(DisjointAbstractionNetwork abn, AbNDisplayManager displayManager) {
+                return new SCTDisjointTANConfigurationFactory().createConfiguration(abn, displayManager);
+            }
+
+            @Override
+            public AbNExplorationPanelGUIInitializer getExplorationGUIInitializer(DisjointAbNConfiguration config) {
+
+                ClusterTribalAbstractionNetwork tan = (ClusterTribalAbstractionNetwork)config.getAbstractionNetwork().getParentAbstractionNetwork();
+
+                return new DisjointAbNExplorationPanelInitializer(
+                        config,
+                        new SCTTANConfigurationFactory().createConfiguration(tan, config.getUIConfiguration().getAbNDisplayManager()),
+                        (bound) -> {
+                            DisjointAbstractionNetwork disjointAbN = config.getAbstractionNetwork().getAggregated(bound);
+                            config.getUIConfiguration().getAbNDisplayManager().displayDisjointTribalAbstractionNetwork(disjointAbN);
+                        },
+                        (disjointAbN) -> {
+                            config.getUIConfiguration().getAbNDisplayManager().displayDisjointTribalAbstractionNetwork(disjointAbN);
+                        });
+            }
+        };
+    }
+}
